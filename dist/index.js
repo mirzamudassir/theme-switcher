c3524317fa7033f2ddc01fab75bec69066411120
Mudassir Mirza <hello.mudassirmirza@gmail.com>
2025-01-29 07:54:57 +0500

    :bug: fixing types and module error on pkg import

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ThemeSwitcher = exports.ThemeProvider = void 0;
-var ThemeProvider_1 = require("./ThemeProvider");
+var ThemeProvider_1 = require("../src/ThemeProvider");
 Object.defineProperty(exports, "ThemeProvider", { enumerable: true, get: function () { return ThemeProvider_1.ThemeProvider; } });
-var ThemeSwitcher_1 = require("./ThemeSwitcher");
+var ThemeSwitcher_1 = require("../src/ThemeSwitcher");
 Object.defineProperty(exports, "ThemeSwitcher", { enumerable: true, get: function () { return ThemeSwitcher_1.ThemeSwitcher; } });
