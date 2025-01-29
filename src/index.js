07e08cb1acb381afc8634be0481d354f9e0642aa
Mudassir Mirza <hello.mudassirmirza@gmail.com>
2025-01-29 07:11:22 +0500

    :wrench: updated readme, patched v1.0.1 to npm registry

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ThemeSwitcher = exports.ThemeProvider = void 0;
+var ThemeProvider_1 = require("./ThemeProvider");
+Object.defineProperty(exports, "ThemeProvider", { enumerable: true, get: function () { return ThemeProvider_1.ThemeProvider; } });
+var ThemeSwitcher_1 = require("./ThemeSwitcher");
+Object.defineProperty(exports, "ThemeSwitcher", { enumerable: true, get: function () { return ThemeSwitcher_1.ThemeSwitcher; } });
